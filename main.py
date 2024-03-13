c2e555abfab46b33e659d8c7b5a7a31c5a4a05e4
pawan_rpa <pawan@rpatechnologies.in>
2024-03-13 18:20:23 +0530

    build snake and food

--- main.py
+++ main.py
@@ -1,43 +1,111 @@
 import pygame
+import time
 from pygame.locals import *
 
-def draw_bloc():
-    surface.fill((110, 110, 5))
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
+size = 40
+
+
+
+class Apple:
+
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("resources/block.jpeg").convert()
+        self.image = pygame.transform.scale(self.image,(40,40))
+        self.parent_screen = parent_screen
+        self.x = size*3
+        self.y = size*3
+    
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+class Snake:
+    def __init__(self, parent_screen, length):
+        self.length = length
+        self.parent_screen = parent_screen
+        self.block = pygame.image.load("resources/block.jpeg").convert()
+        self.block = pygame.transform.scale(self.block,(40,40))
+        self.block_x = [size]*length
+        self.block_y = [size]*length
+        self.direction = 'down'
+
+    def draw(self):
+        self.parent_screen.fill((110, 110, 5))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.block_x[i], self.block_y[i]))
+        pygame.display.flip()
+        
+    def move_left(self):
+        self.direction = 'left'
+
+    def move_right(self):
+        self.direction = 'right'
+
+    def move_up(self):
+        self.direction = 'up'
+
+    def move_down(self):
+        self.direction = 'down'
+
+    def walk(self):
+        for i in range(self.length-1, 0, -1):
+            self.block_x[i] = self.block_x[i-1]
+            self.block_y[i] = self.block_y[i-1]
+        if self.direction == 'down':
+            self.block_y[0] += size
+        if self.direction == 'up':
+            self.block_y[0] -= size
+        if self.direction == 'left':
+            self.block_x[0] -= size
+        if self.direction == 'right':
+            self.block_x[0] += size
+
+        self.draw()
+
+
+        
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.surface  = pygame.display.set_mode((500, 500))
+        self.surface.fill((110, 110, 5))
+        self.snake = Snake(self.surface, 3)
+        self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+
+    def run(self):
+            running = True
+
+            while running:
+                for event in pygame.event.get():
+                  if event.type == KEYDOWN:
+                   if event.key == K_ESCAPE:
+                      running = False
+                   if event.key == K_UP:
+                      self.snake.move_up()
+                   if event.key == K_DOWN:
+                    self.snake.move_down()
+                   if event.key == K_LEFT:
+                    self.snake.move_left()
+                   if event.key == K_RIGHT:
+                    self.snake.move_right()
+                  elif event.type ==  QUIT:
+                      running = False
+                self.play()
+                time.sleep(0.2)
+
 
 
 if __name__ == '__main__':
-    pygame.init()
-    surface  = pygame.display.set_mode((500, 500))
 
+    game = Game()
+    game.run()
 
-    
-    block = pygame.image.load("resources/block.jpeg").convert()
-    block = pygame.transform.scale(block,(35,35) )
-    block_x = 100
-    block_y = 100
-    surface.blit(block, (block_x, block_y))
-    pygame.display.flip()
+    # pygame.display.flip()
 
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    running = False
-                if event.key == K_UP:
-                    block_y -= 10
-                    draw_bloc()
-                if event.key == K_DOWN:
-                    block_y += 10
-                    draw_bloc()
-                if event.key == K_LEFT:
-                    block_x -= 10
-                    draw_bloc()
-                if event.key == K_RIGHT:
-                    block_x += 10
-                    draw_bloc()
-            elif event.type ==  QUIT:
-                running = False
